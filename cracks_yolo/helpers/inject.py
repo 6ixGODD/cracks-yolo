4897ac6be07acc6003838a732d5b438b72c4a24d
6GOD <55304045+6ixGODD@users.noreply.github.com>
2025-10-22 21:48:42 +0800

    feat: add initial inject module for Ultralytics integration

---
+++ cracks_yolo/helpers/inject.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+import typing as t
+
+import torch.nn
+import ultralytics.nn.modules
+import ultralytics.nn.tasks
+
+C = t.TypeVar('C', bound=t.Type[torch.nn.Module])
+
+
+def register_to_ultralytics(cls: C) -> C:
+    module_name = cls.__name__
+    setattr(ultralytics.nn.modules, module_name, cls)  # register to ultralytics.nn.modules
+    if module_name not in ultralytics.nn.modules.__all__:
+        ultralytics.nn.modules.__all__ += (module_name,)  # __all__ is a tuple, strange :(
+
+    setattr(ultralytics.nn.tasks, module_name, cls)  # register to ultralytics.nn.tasks
+
+    return cls
+
+
+
