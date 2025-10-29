3e038d82a66b505c3abec2879390cd968c06ac68
6GOD <55304045+6ixGODD@users.noreply.github.com>
2025-10-29 17:29:26 +0800

    refactor: clean up XML files and improve formatting in configuration files

--- experiments/train.py
+++ experiments/train.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
+# noinspection PyUnusedImports
+from cracks_yolo.nn.modules import *  # noqa: F403
 import ultralytics.nn.modules
-from cracks_yolo.nn.modules import *  # noqa: F401
 
 print(ultralytics.nn.modules.__all__)
 
-model = ultralytics.YOLO("yolov5s-sac-tr.yaml")
-print(model)
+model = ultralytics.YOLO("yolov5s-sac-tr.yaml").load("yolov5su.pt")
+print(model.model)
+print(model.trainer)
+
+model.train(data="../.chore/outputs/cracks-detection/data.yaml")
