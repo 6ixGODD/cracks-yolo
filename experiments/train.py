b0bc48d969d4902c5fff9f5af7fc4a245f71730d
6GOD <55304045+6ixGODD@users.noreply.github.com>
2025-11-10 03:45:43 +0800

    refactor: restructure train.py for improved execution flow and anomaly detection

--- experiments/train.py
+++ experiments/train.py
@@ -1,13 +1,13 @@
 from __future__ import annotations
 
-# noinspection PyUnusedImports
-from cracks_yolo.nn.modules import *  # noqa: F403
+import torch
 import ultralytics.nn.modules
 
-print(ultralytics.nn.modules.__all__)
+if __name__ == "__main__":
+    from cracks_yolo.nn.modules import *  # noqa: F403
 
-model = ultralytics.YOLO("yolov5s-sac-tr.yaml").load("yolov5su.pt")
-print(model.model)
-print(model.trainer)
+    torch.autograd.set_detect_anomaly(True)
+    model = ultralytics.YOLO("yolov5s-sac-tr.yaml").load("yolov5s.pt")
 
-model.train(data="../.chore/outputs/cracks-detection/data.yaml")
+    print(ultralytics.nn.modules.__all__)
+    model.train(data="../.chore/outputs/cracks-detection/data.yaml", epochs=300, batch=12)
