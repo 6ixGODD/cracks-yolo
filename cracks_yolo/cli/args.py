3f827c3d852e021f883ebb2f661a1c81981c6e4a
6GOD <55304045+6ixGODD@users.noreply.github.com>
2025-11-14 21:10:45 +0800

    feat: add BaseArgs class for argument parsing and validation using Pydantic

---
+++ cracks_yolo/cli/args.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import abc
+import argparse
+import typing as t
+
+from pydantic import BaseModel
+
+
+class BaseArgs(BaseModel, abc.ABC):
+    @abc.abstractmethod
+    def run(self) -> None:
+        pass
+
+    @classmethod
+    def func(cls, args: argparse.Namespace) -> None:
+        instance = cls.parse_args(args)
+        instance.run()
+
+    @classmethod
+    @abc.abstractmethod
+    def build_args(cls, parser: argparse.ArgumentParser) -> None:
+        pass
+
+    @classmethod
+    def parse_args(cls, args: argparse.Namespace) -> t.Self:
+        return cls.model_validate(vars(args), strict=True)
