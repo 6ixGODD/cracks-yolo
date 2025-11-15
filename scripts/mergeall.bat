b510167f5424e05a384b32a04885838aca7e9771
6GOD <55304045+6ixGODD@users.noreply.github.com>
2025-11-15 16:40:08 +0800

    chore(*): ...

--- scripts/mergeall.bat
+++ scripts/mergeall.bat
@@ -6,14 +6,13 @@ poetry run python -m cracks_yolo dataset merge ^
     -i data/raw/KN/annotations.json coco KN ^
     -i data/raw/LG/annotations.json coco LG ^
     -i data/raw/SXX/annotations.json coco SXX ^
-    -i data/raw/SZY/annotations_train.json coco SZY ^
     -i data/raw/SZZY/annotations.json coco SZZY ^
-    -o data/cracksdet_coco_701515 ^
-    --output-format coco ^
+    -o data/cracksdet_yolo_701515_BDS-KN-LG-SXX-SZZY ^
+    --output-format yolo ^
     --train-ratio 0.7 ^
     --val-ratio 0.15 ^
     --test-ratio 0.15 ^
-    --naming sequential_prefix
+    --naming sequential_prefix ^
 
 
 endlocal
